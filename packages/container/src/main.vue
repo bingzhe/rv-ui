15ee0d7786bcfb656725c6282bb1f3d374dec49e
bingzhe <874177815@qq.com>
2018-08-31 18:48:42 +0800

    container

--- packages/container/src/main.vue
+++ packages/container/src/main.vue
@@ -1,11 +1,38 @@
 <template>
     <section 
-        class="z-container" >
+        class="z-container" 
+        :class="{'is-vertical': isVertical}"
+    >
         <slot></slot>
     </section>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ZContainer",
+
+  componentName: "ZContainer",
+
+  props: {
+    direction: String
+  },
+
+  computed: {
+    isVertical() {
+      if (this.direction === "vertical") {
+        return true;
+      } else if (this.direction === "horizontal") {
+        return false;
+      }
+
+      return this.$slots && this.$slots.default
+        ? this.$slots.default.some(vnode => {
+            const tag = vnode.componentOptions && vonde.componentOptions.tag;
+            return tag === "z-header" || tag === "z-footer";
+          })
+        : false;
+    }
+  }
+};
 </script>
 
