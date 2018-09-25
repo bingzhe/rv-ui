d1458fd1d589bf11350b4e4404d13cc4de1396a1
bingzhe <874177815@qq.com>
2018-09-25 11:12:45 +0800

    dialog-template

---
+++ packages/modal/src/component.vue
@@ -0,0 +1,34 @@
+<template>
+    <transition
+        name="dialog-fade"
+    >
+        <div class="z-dialog__wrapper">
+            <div class="z-dialog">
+                <div class="z-dialog__header">
+                    <slot name="title">
+                        <span class="z-dialog__title">{{ title }}</span>
+                    </slot>
+                    <button
+                        type="button"
+                        class="z-dialog__headerbtn"
+                    >
+
+                    </button>
+                </div>
+
+                <div class="z-dialog__body">
+                    <slot></slot>
+                </div>
+
+                <div class="z-dialog-footer">
+                    <slot name="footer"></slot>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {};
+</script>
+
